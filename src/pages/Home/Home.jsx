7fdb6bb00f68905f8950bdc92388f5cd3246fd3b
Salvatore Cuomo <salvatore.cuomo96@gmail.com>
2026-06-02 15:38:58 +1000

    refactor: remove close and save buttons from cafe preview banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -324,9 +324,6 @@ export default function Home() {
         {previewCafe && (!sheetVisible || sheetSnap === 0) && !detailCafe && (
           <CafePreviewCard
             cafe={previewCafe}
-            isSaved={isSaved(previewCafe.id)}
-            onToggleSave={toggleSave}
-            onClose={() => setPreviewCafe(null)}
             onOpen={() => setDetailCafe(previewCafe)}
           />
         )}
